d623cca359c390b58ccef4c1197716f98a4c4cde
Per Christian Kvalvik <pc@kvalvik.no>
2025-09-02 14:40:02 +0200

    Add tags to db

--- app/src/db.ts
+++ app/src/db.ts
@@ -6,16 +6,25 @@ export type Product = {
   id: string;
   modelNumber: string;
   name: string;
+  tagIds: string[];
 };
 export type CreateProduct = Omit<Product, "id">;
 
+export type Tag = {
+  id: string;
+  name: string;
+};
+export type CreateTag = Omit<Tag, "id">;
+
 export type Db = {
   products: Product[];
+  tags: Tag[];
 };
 
 export const useDb = () => {
   const [db, setDb] = useLocalStorage<Db>("db");
 
+  // Products
   const products = db?.products || [];
   const addProduct = (product: CreateProduct) => {
     if (JSON.stringify(product).length > MAX_PRODUCT_SIZE) {
@@ -33,10 +42,31 @@ export const useDb = () => {
     });
   };
 
+  // Tags
+  const tags = db?.tags || [];
+  const addTag = (tag: CreateTag) => {
+    if (JSON.stringify(tag).length > MAX_PRODUCT_SIZE) {
+      throw new ValidationError("Tagget er for stort");
+    }
+    setDb({
+      ...db,
+      tags: [...tags, { ...tag, id: createId() }],
+    });
+  };
+  const deleteTag = (id: string) => {
+    setDb({
+      ...db,
+      tags: tags.filter((t) => t.id !== id),
+    });
+  };
+
   return {
     products,
     addProduct,
     deleteProduct,
+    tags,
+    addTag,
+    deleteTag,
   };
 };
 
